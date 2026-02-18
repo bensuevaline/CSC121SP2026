144c629211b41a90846f669a6986e885c6b745b2
Evaline <bensuevalineefe@students.abtech.edu>
2026-02-17 23:56:38 -0500

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,16 @@
+# 4-2 Animals Assignment
+
+animals = ["cat", "rabbit", "koala"]
+
+# Print the names of the animals
+for animal in animals:
+    print(animal)
+
+print("\nStatements about each animal:")
+
+# Print a statement about each animal
+for animal in animals:
+    print("A " + animal + " would make a great pet.")
+
+# Print what these animals have in common
+print("\nAll of these animals are cute and gentle companions!")
