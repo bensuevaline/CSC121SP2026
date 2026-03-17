f58f73e7b959dec7d6d1cd064f9caccf96004f7b
Evaline <bensuevalineefe@students.abtech.edu>
2026-03-17 19:59:26 -0400

    Add files via upload

---
+++ 8-5. Cities/cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country="Turkey"):
+    print(f"{city} is in {country}.")
+
+# Function calls
+describe_city("Istanbul")
+describe_city("Ankara")
+describe_city("Reykjavik", "Iceland")
